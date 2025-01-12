0cf7ce52414db7bcee7c9fd6d6e940df9caa2dae
Alex Xela <h825@pm.me>
2025-01-13 01:31:23 +0700

    Schemas

--- app/actions/appointment.ts
+++ app/actions/appointment.ts
@@ -1,15 +1,61 @@
-import { Appointment } from "../types/appointment";
+import {
+  Appointment,
+  AppointmentPayload,
+  AppointmentPayloadSchema,
+} from "../types/appointment";
+
+import crypto from "crypto";
+import { revalidatePath } from "next/cache";
+
+const appointments: Appointment[] = [];
+
+function __debugRandomError() {
+  const random = Math.random();
+  if (random < 0.2) {
+    throw new Error("500: Internal Server Error");
+  } else if (random < 0.4) {
+    throw new Error("401: Unauthorized");
+  } else if (random < 0.6) {
+    throw new Error("400: Bad Request - Invalid description");
+  }
+}
+
+async function __debugDelay(timeout = 2000) {
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+}
 
 export async function createAppointment(
-  appointment: Omit<Appointment, "id">
-): Promise<{ success: boolean; appointment: Appointment; error?: string }> {
-  throw new Error("Not implemented");
+  appointment: AppointmentPayload
+): Promise<AppointmentPayload> {
+  __debugRandomError();
+
+  const data = {
+    ...AppointmentPayloadSchema.parse(appointment),
+    id: crypto.randomUUID(),
+  };
+
+  appointments.push(data);
+
+  revalidatePath("/");
+  revalidatePath("/appointments");
+  revalidatePath("/make-appointment");
+
+  return data;
 }
 
 export async function getAppointments(): Promise<Appointment[]> {
-  throw new Error("Not implemented");
+  await __debugDelay();
+  return appointments;
 }
 
 export async function getAppointmentDates(): Promise<string[]> {
-  throw new Error("Not implemented");
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  return [
+    ...new Set(
+      appointments.map((app) => {
+        const [day, month, year] = app.date.split("/");
+        return `${day}/${month}/${year}`;
+      })
+    ),
+  ];
 }
--- app/types/appointment.ts
+++ app/types/appointment.ts
@@ -1,7 +1,14 @@
-export interface Appointment {
-  id: string;
-  date: string;
-  time: string;
-  email: string;
-  description?: string;
-}
+import { z } from "zod";
+
+export const AppointmentPayloadSchema = z.object({
+  date: z.date(),
+  email: z.string().email(),
+  description: z.string().optional(),
+});
+
+export const AppointmentSchema = AppointmentPayloadSchema.extend({
+  id: z.string().uuid(),
+});
+
+export type AppointmentPayload = z.infer<typeof AppointmentPayloadSchema>;
+export type Appointment = z.infer<typeof AppointmentSchema>;
